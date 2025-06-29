c1edfabcd6e55a6d77af0f730a9a58576d3e5801
MironBerch <mironberchetov@gmail.com>
2025-06-30 00:26:58 +0300

    Update constants.py

--- ymrp/constants.py
+++ ymrp/constants.py
@@ -1,3 +1,4 @@
+# Date constants
 MONTHS = {
     'января': '01',
     'февраля': '02',
@@ -13,10 +14,12 @@ MONTHS = {
     'декабря': '12',
 }
 
+# Time constants
 SMALL_TIMEOUT = 5_000  # 5 seconds
 MEDIUM_TIMEOUT = 10_000  # 10 seconds
 BIG_TIMEOUT = 15_000  # 15 seconds
 
+# Selector constants
 REVIEWS_CONTAINER = '.business-reviews-card-view__reviews-container'
 REVIEW = '.business-reviews-card-view__review'
 REVIEW_VIEW_EXPAND = '.business-review-view__expand[aria-hidden="false"]'
